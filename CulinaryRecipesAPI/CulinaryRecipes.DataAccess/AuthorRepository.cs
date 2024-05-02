c33b7827acbceece96d9ea14f8c19adcab66ff16
alexa21e <pirvuletu.e.alexandra@gmail.com>
2024-05-02 18:23:28 +0300

    Add Get most prolific authors endpoint

---
+++ CulinaryRecipesAPI/CulinaryRecipes.DataAccess/AuthorRepository.cs
@@ -0,0 +1,30 @@
+﻿using CulinaryRecipes.DataAccess.Abstractions;
+using CulinaryRecipes.Domain;
+using Neo4j.Driver;
+
+namespace CulinaryRecipes.DataAccess
+{
+    public class AuthorRepository: IAuthorRepository
+    {
+        private readonly INeo4JDataAccess _neo4JDataAccess;
+
+        public AuthorRepository(INeo4JDataAccess neo4jDataAccess)
+        {
+            _neo4JDataAccess = neo4jDataAccess;
+        }
+
+        public async Task<List<Author>> GetMostProlificAuthors(int authorsNumber)
+        {
+            var query = $@"MATCH (a:Author)-[:WROTE]->(r:Recipe)
+                           RETURN a.name AS Author, COUNT(r) AS RecipeCount
+                           ORDER BY RecipeCount DESC
+                           LIMIT {authorsNumber}";
+
+            var records = await _neo4JDataAccess.ExecuteReadPropertiesAsync(query, null);
+
+            var authors = records.Select(record => Author.Create(record["Author"].As<string>())).ToList();
+
+            return authors;
+        }
+    }
+}
---
+++ CulinaryRecipesAPI/CulinaryRecipesAPI/Controllers/AuthorsController.cs
@@ -0,0 +1,24 @@
+﻿using CulinaryRecipes.ApplicationServices.Abstractions;
+using CulinaryRecipes.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CulinaryRecipesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly IAuthorService _authorService;
+
+        public AuthorController(IAuthorService authorService)
+        {
+            _authorService = authorService;
+        }
+
+        [HttpGet("mostprolific")]
+        public async Task<ActionResult<List<Author>>> GetMostProlificAuthors(int authorsNumber)
+        {
+            return await _authorService.GetMostProlificAuthors(authorsNumber);
+        }
+    }
+}
